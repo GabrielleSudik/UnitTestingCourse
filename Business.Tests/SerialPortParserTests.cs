dfb3912503a15f96013cd17ed8d685b2fc7bbea7
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2019-06-09 20:04:14 -0400

    first commit of unit testing walkthrough

---
+++ Business.Tests/SerialPortParserTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Tests
+{
+    public class SerialPortParserTests
+    {
+        public void ParsePort_CCOM1_Returns1()
+        {
+            int result = SerialPortParser.ParsePort("COM1");
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+}
